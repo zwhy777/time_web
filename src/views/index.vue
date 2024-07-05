8c08a63ab6cb5d3c5316f94720792d6117b8d88d
dlx <767740759@qq.com>
2024-07-05 11:54:45 +0800

    11

--- src/views/index.vue
+++ src/views/index.vue
@@ -19,6 +19,20 @@
             </el-dropdown>
           </div>
         </div>
+        <div style="margin: 3%; height: 80vh;">
+          <div style="text-align: right;">
+            <el-button color="#c4d4ec"><label class="font">添加行程</label></el-button>
+          </div>
+          <div style="margin-top: 30px; box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);">
+            <el-table :data="tableData" stripe style="width: 100%; height: 70vh;">
+              <el-table-column prop="date" label="日期" width="280" />
+              <el-table-column prop="time" label="时间" width="400" />
+              <el-table-column prop="name" label="行程名称" width="280" />
+              <el-table-column prop="type" label="类型" width="180" />
+              <el-table-column prop="name" label="操作" />
+            </el-table>
+          </div>
+        </div>
 
       </div>
 
@@ -42,6 +56,33 @@ const logout = () => {
   });
   // router.push('/');
 };
+
+const tableData = [
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    time: 'No. 189, Grove St, Los Angeles',
+    type: ''
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    time: 'No. 189, Grove St, Los Angeles',
+    type: ''
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    time: 'No. 189, Grove St, Los Angeles',
+    type: ''
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    time: 'No. 189, Grove St, Los Angeles',
+    type: ''
+  },
+]
 </script>
 
 <style scoped>
@@ -50,6 +91,7 @@ const logout = () => {
   height: 100vh;
   width: 100%;
   background-color: #F2F6FC;
+
 }
 
 .content {
@@ -59,6 +101,7 @@ const logout = () => {
   /* 添加其他样式，如内容区域的填充等 */
   height: 100%;
   width: 100%;
+
 }
 
 .title {
@@ -67,6 +110,12 @@ const logout = () => {
   font-weight: 800;
   padding: 20px;
   border-bottom: 2px solid #303133;
-  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1)
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+}
+
+.font {
+  font-size: 15px;
+  color: #303133;
+  font-weight: 600;
 }
 </style>
--- src/views/week.vue
+++ src/views/week.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="app">
-<!--    <sidebar></sidebar>-->
+    <!--    <sidebar></sidebar>-->
     <div class="content">
       <router-view></router-view>
-      <div>
-        现场处置模块
+      <div class="border">
+        <div class="header_center">
+          <h2>
+            <strong>{{user_name}}--时间管理系统</strong>
+          </h2>
+        </div>
+        <el-button type="primary" style="margin-left: 150px;">主要按钮</el-button>
       </div>
     </div>
   </div>
@@ -14,9 +19,9 @@
 import { get } from "@/net";
 import { ElMessage } from "element-plus";
 import { useRouter } from "vue-router";
-import Sidebar from '../components/sideBar/SideBar.vue';
+import Sidebar from "../components/sideBar/SideBar.vue";
 
-
+const user_name = "";
 </script>
 
 <style scoped>
@@ -28,5 +33,21 @@ import Sidebar from '../components/sideBar/SideBar.vue';
   flex: 1;
   margin-left: 200px; /* 与侧边栏宽度对应 */
   /* 添加其他样式，如内容区域的填充等 */
+}
+
+.header_center {
+  width: 30%;
+  margin: 0px auto;
+  color: black;
+  text-align: center;
+  height: 80px;
+  /* background-image: url("./week_image/logoBg.png");
+  background-size: 100% 100%; */
+  font-family: "微软雅黑" !important;
+  display: inline-block;
+}
+.border {
+  border-bottom: 2px solid #000;
+  text-align: center;
 }
 </style>
